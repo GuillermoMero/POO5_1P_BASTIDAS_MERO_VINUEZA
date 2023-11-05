6f1e9b24ccc3b6b4376e89842ed4df0e1cdea392
GuillermoMero <gimero@espol.edu.ec>
2023-11-05 16:30:03 -0500

    Creacion de la clase Servicio

--- Proyecto1P_Grupo2/src/main/java/Servicio/Servicio.java
+++ Proyecto1P_Grupo2/src/main/java/Servicio/Servicio.java
@@ -9,6 +9,66 @@ package Servicio;
  * @author Paula
  */
 public class Servicio {
+    protected String fecha;
+    protected Cliente cliente;
+    protected String origen;
+    protected String destino;
+    protected Conductor conductor;
+    protected TipoServicio tipoServicio;
     
+    public Servicio(String fecha, Cliente cliente, String origen, String destino, Conductor conductor, TipoServicio tipoServicio){
+        this.fecha = fecha;
+        this.cliente = cliente;
+        this.origen = origen;
+        this.destino = destino;
+        this.conductor = conductor;
+        this.tipoServicio = tipoServicio;
+    }
     
+    public String getFecha(){
+        return this.fecha;
+    }
+    
+    public void setFecha(String fecha){
+        this.fecha = fecha;
+    }
+    public Cliente getCliente(){
+        return this.cliente;
+    }
+    
+    public void setCliente(Cliente cliente){
+        this.cliente = cliente;
+    }
+    
+    public String getOrigen(){
+        return this.origen;
+    }
+    
+    public void setOrigen(String origen){
+        this.origen = origen;
+    }
+    
+    public String getDestino(){
+        return this.destino;
+    }
+    
+    public void setDestino(String destino){
+        this.destino = destino;
+    }
+    
+    public Conductor getConductor(){
+        return this.conductor;
+    }
+    
+    public void setConductor(Conductor conductor){
+        this.conductor = conductor;
+    }
+    
+    public TipoServicio GetTipoServicio(){
+        return this.tipoServicio;
+    }
+    
+    public void setTipoServicio(TipoServicio tipoServiico){
+        this.tipoServicio = tipoServico;
+    }
 }
