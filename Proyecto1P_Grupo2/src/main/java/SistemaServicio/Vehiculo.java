aba3f32eecd30bee46313e9bed641d5606d9c6e9
polipopi <pmbastid@espol.edu.ec>
2023-11-05 16:01:49 -0500

    clase vehiculo

---
+++ Proyecto1P_Grupo2/src/main/java/SistemaServicio/Vehiculo.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SistemaServicio;
+
+/**
+ *
+ * @author Paula
+ */
+public class Vehiculo {
+    private String placa;
+    private String modelo;
+    private String marca;
+    private char tipo;
+    
+    public Vehiculo(String placa, String modelo, String marca, char tipo){
+        this.placa=placa;
+        this.modelo=modelo;
+        this.marca=marca;
+        this.tipo=tipo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+    
+
+}
