81d07de930bab622241d3584bba6d629165ef33a
polipopi <pmbastid@espol.edu.ec>
2023-11-05 16:08:58 -0500

    clase main

--- Proyecto1P_Grupo2/src/main/java/SistemaServicio/sistemaServicio.java
+++ Proyecto1P_Grupo2/src/main/java/SistemaServicio/sistemaServicio.java
@@ -4,12 +4,33 @@
  */
 package SistemaServicio;
 
+import Usuario.Usuario;
+
 /**
  *
  * @author Paula
  */
 public class sistemaServicio {
-    public static void main(String[] args){
-        
+     private Usuario usuario;
+     private String opcion;
+    public static void main(String[] args){   
     }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getOpcion() {
+        return opcion;
+    }
+
+    public void setOpcion(String opcion) {
+        this.opcion = opcion;
+    }
+    
+
 }
