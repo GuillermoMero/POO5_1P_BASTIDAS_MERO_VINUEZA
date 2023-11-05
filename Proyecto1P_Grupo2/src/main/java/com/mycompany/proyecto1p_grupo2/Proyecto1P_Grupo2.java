245c9fcb5f15fe746d2d37c6b03461a815b361bc
GuillermoMero <gimero@espol.edu.ec>
2023-11-04 20:17:57 -0500

    Creacion del proyecto

---
+++ Proyecto1P_Grupo2/src/main/java/com/mycompany/proyecto1p_grupo2/Proyecto1P_Grupo2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1p_grupo2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Proyecto1P_Grupo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
