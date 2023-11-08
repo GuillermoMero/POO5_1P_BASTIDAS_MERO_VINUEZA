3adf6aa1ca9deedb08d5872738fa3a7e1b3da7a5
PedroVinueza <pavinuez@espol.edu.ec>
2023-11-07 21:41:23 -0500

    Creacion de arrayList de Usuarios. PedroVinueza

--- Proyecto1P_Grupo2/src/main/java/SistemaServicio/sistemaServicio.java
+++ Proyecto1P_Grupo2/src/main/java/SistemaServicio/sistemaServicio.java
@@ -4,6 +4,7 @@
  */
 package SistemaServicio;
 
+import Usuario.TipoUsuario;
 import Usuario.Usuario;
 import manejoArchivos.*;
 import java.util.ArrayList;
@@ -12,35 +13,29 @@ import java.util.ArrayList;
  *
  * @author Paula
  */
-public class SistemaServicio {
-    
-    private String opcion;
-    
-    
-    ManejoArchivo manArch = new ManejoArchivo();
-    ArrayList<String> usuarios = manArch.LeeFichero("usuarios.txt");
-    
-    
-    public SistemaServicio(String opcion){
-        this.opcion = opcion;
-        
+public class SistemaServicio {  
+    private static ArrayList<Usuario> usuarios = new ArrayList<>(); 
+    public static void main(String[] args){
+        ArrayList<String> linea = ManejoArchivo.LeeFichero("usuarios.txt");
+        System.out.println(linea);
+        for (int i = 0; i<linea.size();i++){
+            if (i > 0){
+                String[] partes = (linea.get(i)).split(",");
+                Usuario usuario = new Usuario(partes[0],partes[1],partes[2],partes[3],partes[4],partes[5],0,TipoUsuario.valueOf(partes[6]));
+                usuarios.add(usuario);
+                }
+            }
+        System.out.println(usuarios.get(0).getApellidos());
     }
-     
-     
-     
-     
-
-
-
-    
-
-    public String getOpcion() {
-        return opcion;
-    }
-
-    public void setOpcion(String opcion) {
-        this.opcion = opcion;
-    }
-    
-
 }
+     
+     
+     
+     
+
+
+
+
+    
+
+
