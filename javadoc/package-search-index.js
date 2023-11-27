539e3f3e0cfc4f451378c67a3e846a798f062e38
GuillermoMero <gimero@espol.edu.ec>
2023-11-26 22:49:48 -0500

    Reporte y Javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"},{"l":"manejoArchivos"},{"l":"Servicio"},{"l":"SistemaServicio"},{"l":"Usuario"}];updateSearchResults();
